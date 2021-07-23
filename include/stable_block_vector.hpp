a0b9b90f2a4fe45015d0681cf404663c07a817e0
Dmitry Isupov <TheApX@users.noreply.github.com>
2021-07-23 00:05:11 +0000

    Resizing

--- include/stable_block_vector.hpp
+++ include/stable_block_vector.hpp
@@ -1,1 +1,109 @@
 #pragma once
+
+#include <memory>
+#include <stdexcept>
+#include <vector>
+
+namespace theapx {
+
+template <class T, size_t kBlockSize>
+class stable_block_vector {
+ public:
+  class iterator {
+   public:
+    T& operator*() { return owner_->at(pos_); }
+    iterator operator--();
+    iterator operator--(int);
+    iterator operator++();
+    iterator operator++(int);
+
+   private:
+    iterator(stable_block_vector<T, kBlockSize>* owner, size_t index);
+
+    stable_block_vector<T, kBlockSize>* owner_;
+    size_t pos_ = 0;
+  };
+
+  stable_block_vector();
+
+  iterator begin() { return iterator(this, 0); }
+  iterator end() { return iterator(this, size_); }
+
+  T& at(size_t pos);
+  T& operator[](size_t pos) { return at(pos); }
+
+  size_t capacity() const { return capacity_; }
+  size_t size() const { return size_; }
+
+  void reserve(size_t s);
+  void resize(size_t s);
+
+  void push_back(T& v);
+  void push_back(T&& v);
+
+ private:
+  std::vector<std::unique_ptr<std::vector<T>>> blocks_;
+  size_t capacity_ = 0;
+  size_t size_ = 0;
+};
+
+// -----------------------------------------------------------------------------
+
+template <class T, size_t kBlockSize>
+stable_block_vector<T, kBlockSize>::stable_block_vector() {
+  //
+}
+
+template <class T, size_t kBlockSize>
+T& stable_block_vector<T, kBlockSize>::at(size_t pos) {
+  if (pos >= size_) {
+    throw std::out_of_range("pos");
+  }
+
+  size_t block = pos / kBlockSize;
+  size_t block_pos = pos % kBlockSize;
+  return blocks_[block]->at(block_pos);
+}
+
+template <class T, size_t kBlockSize>
+void stable_block_vector<T, kBlockSize>::reserve(size_t s) {
+  size_t blocks_needed = s / kBlockSize + (s % kBlockSize > 0 ? 1 : 0);
+  blocks_.reserve(blocks_needed);
+  while (blocks_.size() < blocks_needed) {
+    std::unique_ptr<std::vector<T>> new_block =
+        std::make_unique<std::vector<T>>();
+    new_block->reserve(kBlockSize);
+    blocks_.push_back(std::move(new_block));
+  }
+}
+
+template <class T, size_t kBlockSize>
+void stable_block_vector<T, kBlockSize>::resize(size_t s) {
+  if (s == size_) {
+    return;
+  }
+
+  reserve(s);
+
+  size_t old_full_blocks_count = size_ / kBlockSize;
+  size_t old_block_pos = size_ % kBlockSize;
+  size_t new_full_blocks_count = s / kBlockSize;
+  size_t new_block_pos = s % kBlockSize;
+
+  if (new_full_blocks_count > old_full_blocks_count) {
+    for (size_t b = old_full_blocks_count; b < new_full_blocks_count; ++b) {
+      blocks_[b]->resize(kBlockSize);
+    }
+  }
+
+  if (new_full_blocks_count < old_full_blocks_count) {
+    for (size_t b = old_full_blocks_count; b > new_full_blocks_count; --b) {
+      blocks_[b]->resize(0);
+    }
+  }
+
+  blocks_[new_full_blocks_count]->resize(new_block_pos);
+  size_ = s;
+}
+
+}  // namespace theapx
--- test/stable_block_vector_test.cpp
+++ test/stable_block_vector_test.cpp
@@ -5,9 +5,120 @@
 
 namespace {
 
-TEST(StableBlockVectorTest, BuildsAndLinks) {
-  EXPECT_THAT(1, testing::Eq(1));
-  EXPECT_THAT(2, testing::Ne(1));
+using ::testing::Eq;
+
+class TestObject {
+ public:
+  TestObject() { ++constructor_counter; }
+  TestObject(const TestObject& other) { ++constructor_counter; }
+  TestObject(TestObject&& other) { ++constructor_counter; }
+
+  ~TestObject() { ++destructor_counter; }
+
+  static void ResetCounters() {
+    constructor_counter = 0;
+    destructor_counter = 0;
+  }
+
+  static int constructor_counter;
+  static int destructor_counter;
+};
+
+int TestObject::constructor_counter = 0;
+int TestObject::destructor_counter = 0;
+
+TEST(StableBlockVectorTest, GrowWithinFirstBlock) {
+  TestObject::ResetCounters();
+  {
+    theapx::stable_block_vector<TestObject, 5> v;
+
+    v.resize(1);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(1));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+
+    v.resize(3);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(3));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+  }
+  EXPECT_THAT(TestObject::constructor_counter, Eq(3));
+  EXPECT_THAT(TestObject::destructor_counter, Eq(3));
+}
+
+TEST(StableBlockVectorTest, ShrinkWithinFirstBlock) {
+  TestObject::ResetCounters();
+  {
+    theapx::stable_block_vector<TestObject, 5> v;
+
+    v.resize(3);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(3));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+
+    v.resize(1);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(3));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(2));
+  }
+  EXPECT_THAT(TestObject::constructor_counter, Eq(3));
+  EXPECT_THAT(TestObject::destructor_counter, Eq(3));
+}
+
+TEST(StableBlockVectorTest, GrowToSecondBlock) {
+  TestObject::ResetCounters();
+  {
+    theapx::stable_block_vector<TestObject, 5> v;
+
+    v.resize(1);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(1));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+
+    v.resize(8);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(8));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+  }
+  EXPECT_THAT(TestObject::constructor_counter, Eq(8));
+  EXPECT_THAT(TestObject::destructor_counter, Eq(8));
+}
+
+TEST(StableBlockVectorTest, ShrinkFromSecondBlock) {
+  TestObject::ResetCounters();
+  {
+    theapx::stable_block_vector<TestObject, 5> v;
+
+    v.resize(8);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(8));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(0));
+
+    v.resize(1);
+    EXPECT_THAT(TestObject::constructor_counter, Eq(8));
+    EXPECT_THAT(TestObject::destructor_counter, Eq(7));
+  }
+  EXPECT_THAT(TestObject::constructor_counter, Eq(8));
+  EXPECT_THAT(TestObject::destructor_counter, Eq(8));
+}
+
+TEST(StableBlockVectorTest, PointersStable) {
+  TestObject::ResetCounters();
+
+  theapx::stable_block_vector<TestObject, 5> v;
+
+  const int max_count = 100;
+  std::vector<TestObject*> addresses(max_count, nullptr);
+
+  for (int i = 1; i < max_count; ++i) {
+    v.resize(i);
+    addresses[i - 1] = &v[i - 1];
+    for (int j = 0; j < i; ++j) {
+      EXPECT_THAT(addresses[j], Eq(&v[j])) << " i = " << i << "  j = " << j;
+    }
+  }
+
+  v.resize(0);
+
+  for (int i = 1; i < max_count; ++i) {
+    v.resize(i);
+    for (int j = 0; j < i; ++j) {
+      EXPECT_THAT(addresses[j], Eq(&v[j])) << " i = " << i << "  j = " << j;
+    }
+  }
 }
 
 }  // namespace
